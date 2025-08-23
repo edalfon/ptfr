2a835488e0c43455dfa3efe95df6587e0968345a
edalfon <edalfon@gmail.com>
2025-08-23 12:25:41 +0200

    feat: capacitacion raices

--- _targets.R
+++ _targets.R
@@ -4,6 +4,7 @@ list(
   plan_sample_selection(),
   tar_quarto(capacita_slides, "slides/capacitacion.qmd"),
   tar_quarto(capacita_slides_sirenas, "slides/capacitacion_Sirenas_PuertoSaija.qmd"),
+  tar_quarto(capacita_raices, "slides/capacitacion_raices.qmd"),
 
   NULL
   # flowme::tar_bookdown("report")
